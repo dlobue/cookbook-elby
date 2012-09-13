252f14b17aa5f74a4aecb71f1012feb0ce28bf67
Dominic LoBue <dominic.lobue@gmail.com>
2012-09-13 11:51:03 -0700

    cruft cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,5 @@
 
-include_attribute "solo_client::default"
-
 default.elby.wait_for_healthy = false
 
-default.deployment[:elbs] = {}
-default.deployment[:elbs][:appdb] = nil
-default.deployment[:elbs][:openpub] = nil
-default.deployment[:elbs][:minipax] = nil
-default.deployment[:elbs]["gd-app"] = nil
-default.deployment[:elbs]["cb-apache"] = nil
+default.deployment.elbs = Mash.new
 
